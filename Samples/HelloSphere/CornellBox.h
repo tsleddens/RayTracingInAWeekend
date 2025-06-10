cb17eb07819bf4f3fca9b8cbf31b0d1acc996381
Tim <timsleddens@hotmail.com>
2025-06-10 23:27:24 +0100

    Add CornellBox sample

---
+++ Samples/HelloSphere/CornellBox.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "tsleddens/AppBase.h"
+#include "tsleddens/Quad.h"
+#include "tsleddens/Materials/DiffuseLight.h"
+#include "tsleddens/Materials/Lambertian.h"
+#include "tsleddens/Textures/ImageTexture.h"
+
+using namespace tsleddens;
+
+class CornellBox final : public AppBase
+{
+    std::unique_ptr<IMaterial> m_red;
+    std::unique_ptr<IMaterial> m_white;
+    std::unique_ptr<IMaterial> m_green;
+    std::unique_ptr<IMaterial> m_light;
+
+public:
+
+    CornellBox(int width, int height, const wchar_t* title) :
+        AppBase(width, height, title),
+        m_red(std::make_unique<Lambertian>(.65f, .05f, .05f)),
+        m_white(std::make_unique<Lambertian>(.73f)),
+        m_green(std::make_unique<Lambertian>(.12f, .45f, .15f)),
+        m_light(std::make_unique<DiffuseLight>(15.f))
+    {
+    }
+
+    ~CornellBox() override = default;
+
+protected:
+
+    void InitWorld(World& world) override
+    {
+        auto camera = GetCamera();
+        camera->SetPosition(278.f, 278.f, -800.f);
+        camera->SetLookAt(278.f, 278.f, 0.f);
+        camera->SetVerticalFieldOfView(40.f);
+
+        world.AddObject<Quad>(Point3(555.f, 0.f, 5.f), Vector3(0.f, 555.f, 0.f), Vector3(0.f, 0.f, 555.f), m_green.get());
+        world.AddObject<Quad>(Point3(0.f), Vector3(0.f, 555.f, 0.f), Vector3(0.f, 0.f, 555.f), m_red.get());
+        world.AddObject<Quad>(Point3(343.f, 554.f, 332.f), Vector3(-130.f, 0.f, 0.f), Vector3(0.f, 0.f, -105.f), m_light.get());
+        world.AddObject<Quad>(Point3(0.f), Vector3(555.f, 0.f, 0.f), Vector3(0.f, 0.f, 555.f), m_white.get());
+        world.AddObject<Quad>(Point3(555.f), Vector3(-555.f, 0.f, 0.f), Vector3(0.f, 0.f, -555.f), m_white.get());
+        world.AddObject<Quad>(Point3(0.f, 0.f, 554.9999f), Vector3(555.f, 0.f, 0.f), Vector3(0.f, 555.f, 0.f), m_white.get());
+    }
+};
